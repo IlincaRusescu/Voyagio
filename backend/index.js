5e09bdef9babd44d0366d501802557f4e499260f
ilinq01 <89912932+ilinq01@users.noreply.github.com>
2025-11-18 12:15:06 +0200

    Initial project setup

---
+++ backend/index.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const cors = require('cors')
+const PORT = 3000;
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Voyagio API is running");
+});
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
