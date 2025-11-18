da34739f163c1742b1c9401e1bd2ce730e6522f0
ilinq01 <89912932+ilinq01@users.noreply.github.com>
2025-11-18 12:43:37 +0200

    Day 1: backend & frontend setup + successful connection

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,9 +1,27 @@
-<script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
+<script setup>
+import { onMounted } from 'vue'
+import { api } from '../api'
+
+onMounted(async () => {
+  try {
+    const res = await api.get('/')
+    console.log(res.data)
+  } catch (err) {
+    console.error('API error:', err)
+  }
+})
 </script>
 
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  <div class="home">
+    <h1>Voyagio</h1>
+    <p>Testing backend connection...</p>
+  </div>
 </template>
+
+<style scoped>
+.home {
+  padding: 2rem;
+  color: white;
+}
+</style>
